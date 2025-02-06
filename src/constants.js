35c5515b8c54a315733191640ccbed97a44179dd
Fredrik Mohag Håkansson <frehak54268@stud.noroff.no>
2025-02-06 20:53:05 +0100

    added constants

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const API_KEY = "7fcfd799-4542-4a13-9366-00e2526935b0";
+
+export const API_HOST_URL = ".api.noroff.dev";
+export const API_VERSION = "v2";
+export const API_BASE_URL = `https://${API_VERSION}${API_HOST_URL}`;
+export const API_HOLIDAZE_BASE = "/holidaze";
+export const API_HOLIDAZE_URL = `https://${API_VERSION}${API_HOST_URL}${API_HOLIDAZE_BASE}`;
