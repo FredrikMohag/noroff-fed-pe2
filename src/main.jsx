d6d17665c37d564a6e869f24da4426e94847b1e8
Fredrik Mohag Håkansson <frehak54268@stud.noroff.no>
2025-02-06 20:50:51 +0100

    main & app build

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+//import store from "./store.js";
+import './styles/global.scss';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+const rootElement = document.getElementById("root");
+
+
+ReactDOM.createRoot(rootElement).render(
+
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
+
 );
+//<Provider store={store}>
+
+//</Provider>
