aea52891b67a76c83ee4a699156b9fde7fe9870e
Fredrik Mohag Håkansson <frehak54268@stud.noroff.no>
2025-02-09 15:50:04 +0100

    update with background image. added imports

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,12 +1,20 @@
-// src/pages/Auth.jsx
 import React from "react";
+import { useSelector } from "react-redux";
 import Layout from "../components/Layout";
+import AuthContainer from "../features/auth/AuthContainer";
+import ProfileDetails from "../features/profile/ProfileDetails";
+
 
 function Auth() {
+  // Hämta autentiseringsstatus från Redux store
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Layout>
-      <h1>Login / Register</h1>
-      {/* Lägg till innehåll för login och register */}
+
+      <div className="background-image" />
+      {isAuthenticated ? <ProfileDetails /> : <AuthContainer />}
+
     </Layout>
   );
 }
