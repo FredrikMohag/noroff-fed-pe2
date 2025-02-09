9473328455b75c9503846981e473f4a1b5a3a26b
Fredrik Mohag Håkansson <frehak54268@stud.noroff.no>
2025-02-09 23:41:14 +0100

    make the navbar switch to profile-mode after login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const user = useSelector((state) => state.auth.user); // Kolla om användaren är inloggad
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -9,10 +12,18 @@ function Navbar() {
         <Link to="/" className="navbar-logo">
           <img src="/logo.svg" alt="Logo" className="logo" />
         </Link>
+
         {/* Navigationslänkar */}
         <ul className="navbar-list">
-
-          <li><Link to="/login" className="navbar-link">Profile</Link></li>
+          {user ? (
+            <li>
+              <Link to="/profile" className="navbar-link">Profile</Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login" className="navbar-link">Login</Link>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
