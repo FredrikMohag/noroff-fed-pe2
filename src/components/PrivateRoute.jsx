c65ddbd573625c2e09b4d56bef8a1b4162439c04
Fredrik Mohag Håkansson <frehak54268@stud.noroff.no>
2025-02-16 12:30:52 +0100

    added private route

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+// components/PrivateRoute.jsx
+
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ isAuthenticated, children }) => {
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
+
+
+export default PrivateRoute;
