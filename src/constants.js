df3a8a20eb4e869e8beba699ac0b861c7d573a49
Fredrik Mohag Håkansson <frehak54268@stud.noroff.no>
2025-02-16 12:28:03 +0100

    updated my api endpoints

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,12 @@
-export const API_KEY = "7fcfd799-4542-4a13-9366-00e2526935b0";
+export const BASE_API_URL = 'https://v2.api.noroff.dev';
 
-export const API_HOST_URL = ".api.noroff.dev";
-export const API_VERSION = "v2";
-export const API_BASE_URL = `https://${API_VERSION}${API_HOST_URL}`;
-export const API_HOLIDAZE_BASE = "/holidaze";
-export const API_HOLIDAZE_URL = `https://${API_VERSION}${API_HOST_URL}${API_HOLIDAZE_BASE}`;
+export const API_AUTH = '/auth';
+export const API_LOGIN = '/login';
+export const API_REGISTER = '/register';
+export const API_PROFILE = '/holidaze/profiles';
+export const API_VENUES = '/holidaze/venues';
+export const API_BOOKINGS = '/holidaze/bookings';
+
+export const API_KEY = "35d2b29a-012d-437d-83a5-0e67b63e0d8b";
+
+//export const API_KEY = "7fcfd799-4542-4a13-9366-00e2526935b0";
